47d70738158b928a46811062ddfea32f84333f00
alaaelsayedmohamed <121302082+alaaelsayedmohamed@users.noreply.github.com>
2022-12-23 22:49:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace perfect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Ent num1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ent num2");
+            num2 = int.Parse(Console.ReadLine());
+            for (int i = num1; i <= num2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == i)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
